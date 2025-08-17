9b8bedd103e04cdf77bf01bd39baf53557d96518
indianelephant <elephantindian0117@gmail.com>
2025-08-17 14:28:40 +0900

    HelloWorldの処理追加 * 画面表示時は"initial"を表示 * ボタンを押下でFlutterモジュールから取得した情報を表示(仮)

--- imitate/imitate/ContentView.swift
+++ imitate/imitate/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var viewModel = ContentViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text(viewModel.helloWorldText)
+            Button("update") {
+                Task {
+                    await viewModel.loadHelloWorld()
+                }
+            }
         }
         .padding()
+    }
+}
+
+class ContentViewModel: ObservableObject {
+    @Published var helloWorldText = "initial Hello World"
+    private let hellowWorldRepository = HellowWorldRepository()
+    
+    func loadHelloWorld() async {
+        do {
+            helloWorldText = try await hellowWorldRepository.fetch()
+        } catch {
+            helloWorldText = "failed loadHelloWorld()"
+        }
+    }
+}
+
+class HellowWorldRepository {
+    
+    func fetch() async throws -> String {
+        // TODO: ここをFlutterモジュールから表示状態を取得するコードに変更
+        return "Hello World"
     }
 }
 
