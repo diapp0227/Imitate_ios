c05e7646a712ca9b6dac1e5d967a2fba356934e8
indianelephant <elephantindian0117@gmail.com>
2025-08-17 16:53:07 +0900

    AppDelegate・SceneDelegateを作成して追加

---
+++ imitate/imitate/SceneDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  SceneDelegate.swift
+//  imitate
+//
+//  Created by garigari0118 on 2025/08/17.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    }
+
+
+}
+
--- imitate/imitate/imitateApp.swift
+++ imitate/imitate/imitateApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct imitateApp: App {
+    
+    @UIApplicationDelegateAdaptor (AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
